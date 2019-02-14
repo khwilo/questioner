ffb2a63b55dcc5db3d56ee966846b536b607f1a1
khwilo <khwilowatai@gmail.com>
2019-02-14 13:09:13 +0300

    [Feature #163972787] Implement logic for the user login action

--- UI/js/login.js
+++ UI/js/login.js
@@ -1,5 +1,20 @@
 const userSignInForm = document.getElementById('user-sign-in');
 
+const modal        = document.querySelector(".modal");
+const closeDisplay = document.querySelector(".close-display");
+const modalDisplay = document.querySelector(".modal-display");
+
+const toggleModal = (data) => {
+    modalDisplay.innerHTML = `${data}`;
+    modal.classList.toggle("show-modal");
+};
+
+const windowOnClick = (event) => {
+    if (event.target === modal) {
+        toggleModal();
+    }
+};
+
 const logInUser = (e) => {
     e.preventDefault();
 
@@ -16,8 +31,18 @@ const logInUser = (e) => {
         }
     })
     .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+    .then(data => {
+        if (data.status == 200) {
+            window.location.href = "explore.html";
+        } else {
+            toggleModal(data.message.error);
+        }
+    })
+    .catch(_err => {
+        toggleModal("There's a problem connecting to the server. Email khwilowatai@gmail.com for further assistance.");
+    });
 };
 
 userSignInForm.addEventListener('submit', logInUser);
+closeDisplay.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
