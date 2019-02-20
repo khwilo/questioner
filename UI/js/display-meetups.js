32eccaef76d3356f868c957d8bb2f5338668d1f4
khwilo <khwilowatai@gmail.com>
2019-02-20 13:29:44 +0300

    [Feature #164102224] Implement the logic to display meetups

---
+++ UI/js/display-meetups.js
@@ -0,0 +1,27 @@
+const meetUpTimeline = document.getElementById('meetup-timeline-display');
+
+fetch('http://127.0.0.1:5000/api/v2/meetups/upcoming/', {
+    headers: {
+        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+    }
+})
+.then(res => res.json())
+.then(data => {
+    let meetups    = data.data;
+    let meetupData = '';
+    meetups.forEach(meetup => {
+        meetupData += `
+            <div class="meetup-feed meetup-post">
+                <div class="meetup-title">
+                    <p>${meetup.topic}</p>
+                </div>
+                <div class="meetup-date-time">
+                    <p>${meetup.happeningOn}</p>
+                </div>
+                <button class="meetup-join-btn" onclick="location.href='meetup.html'">JOIN</button>
+            </div>
+        `;
+    });
+    meetUpTimeline.innerHTML += meetupData;
+})
+.catch(err => console.log(err));
