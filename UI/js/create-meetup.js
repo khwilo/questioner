b7de7808ff0e75e979d7419f92b4be794fc1a119
khwilo <khwilowatai@gmail.com>
2019-02-20 11:15:10 +0300

    [Feature #164025992] Implement logic to display an appropriate message on meetup creation

--- UI/js/create-meetup.js
+++ UI/js/create-meetup.js
@@ -1,5 +1,19 @@
 const createMeetupForm = document.getElementById('create-meetup');
-const clickBtn = document.getElementById('click-me');
+
+const modal        = document.querySelector(".modal");
+const closeDisplay = document.querySelector(".close-display");
+const modalDisplay = document.querySelector(".modal-display");
+
+const toggleModal = (data) => {
+    modalDisplay.innerHTML = `${data}`;
+    modal.classList.toggle("show-modal");
+};
+
+const windowOnClick = (event) => {
+    if (event.target === modal) {
+        toggleModal();
+    }
+};
 
 const createMeetup = (e) => {
     e.preventDefault();
@@ -25,8 +39,18 @@ const createMeetup = (e) => {
         }
     })
     .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+    .then(data => {
+        if (data.status == 201) {
+            toggleModal(data.message);
+        } else {
+            toggleModal(data.message.error);
+        }
+    })
+    .catch(err => {
+        toggleModal(err.message + ". Email khwilowatai@gmail.com for further assistance.");
+    });
 };
 
 createMeetupForm.addEventListener('submit', createMeetup);
+closeDisplay.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
